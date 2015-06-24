1840923fc37f0bed6765ac34a9d78ab8872b6934
Nicolas Fantone <nfantone@contentamobile.com>
2015-06-24 12:08:53 -0300

    Updates registry logic to https://github.com/phated/undertaker/tree/registry-init#sharing-tasks

--- index.js
+++ index.js
@@ -1,29 +1,31 @@
 'use strict';
 
-var gulp = require('gulp');
 var util = require('util');
 var Gitflow = require('./lib/gitflow');
 var DefaultRegistry = require('undertaker-registry');
 
 var GitflowRegistry = (function () {
+
 	function GitflowRegistry(options) {
+		this.options = options;
 		DefaultRegistry.call(this);
-
-		var gitflow = new Gitflow(options);
-		var recipes = {release: {}};
-
-		recipes.bump = gitflow.bump;
-		recipes.release.start = gulp.series(gitflow.bump, gitflow.branch, gitflow.commit);
-		recipes.release.finish = gulp.series(gitflow.merge, gitflow.tag, gitflow.merge,
-			gitflow.bumpdev, gitflow.commit, gitflow.clean, gitflow.push);
-
-		this.set('bump', recipes.bump);
-		this.set('release-start', recipes.release.start);
-		this.set('release-finish', recipes.release.finish);
-
 	}
 
 	util.inherits(GitflowRegistry, DefaultRegistry);
+
+	GitflowRegistry.prototype.init = function init(taker) {
+		var gitflow = new Gitflow(this.options);
+
+		var recipes = {release: {}};
+		recipes.bump = gitflow.bump;
+		recipes.release.start = taker.series(gitflow.bump, gitflow.branch, gitflow.commit);
+
+		recipes.release.finish = taker.series(gitflow.merge, gitflow.tag, gitflow.merge,
+			gitflow.bumpdev, gitflow.commit, gitflow.clean, gitflow.push);
+		taker.task('bump', recipes.bump);
+		taker.task('release-start', recipes.release.start);
+		taker.task('release-finish', recipes.release.finish);
+	};
 
 	return GitflowRegistry;
 })();
